a6571d45d6489bb469c65a84c991e800a1780d04
anderjuli <julioauza.a@northeastern.edu>
2021-03-31 08:57:02 -0400

    Added getters and setters to RGB class

--- src/cs5004/animator/RGB.java
+++ src/cs5004/animator/RGB.java
@@ -5,6 +5,18 @@ public class RGB {
   public double green;
   public double blue;
 
+  public double getRed() {
+    return red;
+  }
+
+  public double getGreen() {
+    return green;
+  }
+
+  public double getBlue() {
+    return blue;
+  }
+
   public RGB (double red, double green, double blue) {
     if (red < 0 && red > 255) {
       throw new IllegalArgumentException("Red has to be within 0-255 range.");
@@ -25,6 +37,20 @@ public class RGB {
       this.blue = blue;
     }
   }
+
+  public void setRed(double red) {
+    this.red = red;
+  }
+
+  public void setGreen(double green) {
+    this.green = green;
+  }
+
+  public void setBlue(double blue) {
+    this.blue = blue;
+  }
+
+  public double
 
 
 }
