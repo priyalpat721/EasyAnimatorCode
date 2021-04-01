bbea6a31c2b287b4b2a269d5109a7d72e1c550dc
Priyal <priyalpat721@gmail.com>
2021-04-01 16:09:48 -0400

    java docs

--- src/cs5004/animator/IAction.java
+++ src/cs5004/animator/IAction.java
@@ -6,13 +6,23 @@ package cs5004.animator;
 public interface IAction {
 
   /**
-   *
-   * @return
+   *Gets the start time of the event.
+   * @return an int for the starting time.
    */
   int getStartTime();
 
+  /**
+   *Gets the end time of the event.
+   * @return an int for the ending time.
+   */
   int getEndTime();
 
+  /**
+   * A function that returns the state of the object at a particular tick. The object's state
+   * is in proportion to where the tick is in the action's interval (between start and end time).
+   * @param tick an int that represents the frame of the animation per unit of time.
+   * @param accumulatorShape
+   * @return
+   */
   IShape getShapeAtTick(int tick, IShape accumulatorShape);
-
 }
