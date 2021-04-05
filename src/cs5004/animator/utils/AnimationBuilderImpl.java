0482e1fad009313028e3c7c0673c00253a27cce8
cerulij <Polik010290>
2021-04-05 18:11:22 -0400

    Update

--- src/cs5004/animator/utils/AnimationBuilderImpl.java
+++ src/cs5004/animator/utils/AnimationBuilderImpl.java
@@ -20,7 +20,10 @@ public class AnimationBuilderImpl implements AnimationBuilder<IAnimatorModel> {
   }
 
   @Override
-  public AnimationBuilder<IAnimatorModel> addMotion(String name, int t1, int x1, int y1, int w1, int h1, int r1, int g1, int b1, int t2, int x2, int y2, int w2, int h2, int r2, int g2, int b2) {
+  public AnimationBuilder<IAnimatorModel> addMotion(String name, int t1, int x1, int y1,
+                                                    int w1, int h1, int r1, int g1, int b1,
+                                                    int t2, int x2, int y2, int w2, int h2,
+                                                    int r2, int g2, int b2) {
     return null;
   }
 }
