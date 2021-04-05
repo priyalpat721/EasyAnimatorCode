dff884a17acda4c93520af67f9a19de5a75671ff
cerulij <Polik010290>
2021-04-05 17:40:02 -0400

    Update

--- src/cs5004/animator/EasyAnimator.java
+++ src/cs5004/animator/EasyAnimator.java
@@ -2,7 +2,7 @@ package cs5004.animator;
 
 public final class EasyAnimator {
   public static void main(String[] args) {
-    // Entry point of our program
+    // Entry point to our program
 
   }
 
