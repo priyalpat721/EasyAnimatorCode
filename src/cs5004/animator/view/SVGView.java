51b34309c5cd3a75c48af206f61e612de91fe0c6
cerulij <Polik010290>
2021-04-08 10:40:36 -0400

    Update

--- src/cs5004/animator/view/SVGView.java
+++ src/cs5004/animator/view/SVGView.java
@@ -1,5 +1,7 @@
 package cs5004.animator.view;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +10,10 @@ import cs5004.animator.action.IAction;
 import cs5004.animator.model.IAnimatorModel;
 import cs5004.animator.shape.IShape;
 import cs5004.animator.shape.Shape;
+import cs5004.animator.utils.AnimationBuilder;
+import cs5004.animator.utils.Builder;
+
+import static cs5004.animator.utils.AnimationReader.parseFile;
 
 public class SVGView {
   private StringBuilder result;
@@ -17,19 +23,20 @@ public class SVGView {
   }
 
   public void buildSVG(IAnimatorModel model) {
+
     List<IAction> actions = model.getChronological();
     HashMap<String, IShape> shapes = model.getLogOfShapes();
 
-    result.append(String.format("<svg width='%d' height='%d' "
-                    + "version='1.1' xmlns='http://www.w3.org/2000/svg'>\n\n", model.getBox()[2],
+    result.append(String.format("<svg width=\"%d\" height=\"%d\" "
+                    + "version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n\n", model.getBox()[2],
                                                                                model.getBox()[3]));
 
 
     for (Map.Entry<String, IShape> entry : shapes.entrySet()) {
       if (entry.getValue().getType() == Shape.RECTANGLE) {
         IShape shape = entry.getValue();
-        result.append(String.format("<rect id='%s' x='%d' y='%d' width='%d' height='%d' "
-                + "fill=rgb('%d','%d','%d') visibility='visible' >\n", shape.getName(),
+        result.append(String.format("<rect id=\"%s\" x=\"%d\" y=\"%d\" width=\"%d\" height=\"%d\" "
+                + "fill=rgb(\"%d\",\"%d\",\"%d\") visibility=\"visible\" >\n", shape.getName(),
                                                                (int) shape.getPosition().getX(),
                                                                (int) shape.getPosition().getY(),
                                                                (int) shape.getWidth(),
@@ -41,58 +48,58 @@ public class SVGView {
           if (action.getName().equals(shape.getName())) {
             switch (action.getType()) {
               case MOVE:
-                result.append(String.format("\t<animate attributeType='xml' begin='%s' dur='%s' "
-                        + "attributeName='x' from='%d' to='%d' fill='freeze' />\n",
-                        action.getTime().getStartTime() * 1000 + "ms",
-                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 1000 + "ms",
+                result.append(String.format("\t<animate attributeType=\"xml\" begin=\"%s\" dur=\"%s\" "
+                        + "attributeName=\"x\" from=\"%d\" to=\"%d\" fill=\"freeze\" />\n",
+                        action.getTime().getStartTime() * 100 + "ms",
+                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 100 + "ms",
                         (int) action.getCurrentShape().getPosition().getX(),
                         (int) action.getNewPosition().getX()));
 
-                result.append(String.format("\t<animate attributeType='xml' begin='%s' dur='%s' "
-                                + "attributeName='y' from='%d' to='%d' fill='freeze' />\n",
-                        action.getTime().getStartTime() * 1000 + "ms",
-                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 1000 + "ms",
+                result.append(String.format("\t<animate attributeType=\"xml\" begin=\"%s\" dur=\"%s\" "
+                                + "attributeName=\"y\" from=\"%d\" to=\"%d\" fill=\"freeze\" />\n",
+                        action.getTime().getStartTime() * 100 + "ms",
+                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 100 + "ms",
                         (int) action.getCurrentShape().getPosition().getY(),
                         (int) action.getNewPosition().getY()));
 
                 break;
 
               case SCALE:
-                result.append(String.format("\t<animate attributeType='xml' begin='%s' dur='%s' "
-                        + "attributeName='width' from='%d' to='%d' fill='freeze' />\n",
-                        action.getTime().getStartTime() * 1000 + "ms",
-                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 1000 + "ms",
+                result.append(String.format("\t<animate attributeType=\"xml\" begin=\"%s\" dur=\"%s\" "
+                                + "attributeName=\"width\" from=\"%d\" to=\"%d\" fill=\"freeze\" />\n",
+                        action.getTime().getStartTime() * 100 + "ms",
+                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 100 + "ms",
                         (int) action.getCurrentShape().getWidth(),
                         (int) action.getNewWidth()));
 
-                result.append(String.format("\t<animate attributeType='xml' begin='%s' dur='%s' "
-                                + "attributeName='height' from='%d' to='%d' fill='freeze' />\n",
-                        action.getTime().getStartTime() * 1000 + "ms",
-                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 1000 + "ms",
+                result.append(String.format("\t<animate attributeType=\"xml\" begin=\"%s\" dur=\"%s\" "
+                                + "attributeName=\"height\" from=\"%d\" to=\"%d\" fill=\"freeze\" />\n",
+                        action.getTime().getStartTime() * 100 + "ms",
+                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 100 + "ms",
                         (int) action.getCurrentShape().getHeight(),
                         (int) action.getNewHeight()));
 
                 break;
 
               case CHANGECOLOR:
-                result.append(String.format("\t<animate attributeType='xml' begin='%s' dur='%s' "
-                                + "attributeName='r' from='%d' to='%d' fill='freeze' />\n",
-                        action.getTime().getStartTime() * 1000 + "ms",
-                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 1000 + "ms",
+                result.append(String.format("\t<animate attributeType=\"xml\" begin=\"%s\" dur=\"%s\" "
+                                + "attributeName=\"r\" from=\"%d\" to=\"%d\" fill=\"freeze\" />\n",
+                        action.getTime().getStartTime() * 100 + "ms",
+                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 100 + "ms",
                         (int) action.getCurrentShape().getColor().getRed(),
                         (int) action.getNewColor().getRed()));
 
-                result.append(String.format("\t<animate attributeType='xml' begin='%s' dur='%s' "
-                                + "attributeName='g' from='%d' to='%d' fill='freeze' />\n",
-                        action.getTime().getStartTime() * 1000 + "ms",
-                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 1000 + "ms",
+                result.append(String.format("\t<animate attributeType=\"xml\" begin=\"%s\" dur=\"%s\" "
+                                + "attributeName=\"g\" from=\"%d\" to=\"%d\" fill=\"freeze\" />\n",
+                        action.getTime().getStartTime() * 100 + "ms",
+                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 100 + "ms",
                         (int) action.getCurrentShape().getColor().getGreen(),
                         (int) action.getNewColor().getGreen()));
 
-                result.append(String.format("\t<animate attributeType='xml' begin='%s' dur='%s' "
-                                + "attributeName='b' from='%d' to='%d' fill='freeze' />\n",
-                        action.getTime().getStartTime() * 1000 + "ms",
-                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 1000 + "ms",
+                result.append(String.format("\t<animate attributeType=\"xml\" begin=\"%s\" dur=\"%s\" "
+                                + "attributeName=\"b\" from=\"%d\" to=\"%d\" fill=\"freeze\" />\n",
+                        action.getTime().getStartTime() * 100 + "ms",
+                        (action.getTime().getEndTime() - action.getTime().getStartTime()) * 100 + "ms",
                         (int) action.getCurrentShape().getColor().getBlue(),
                         (int) action.getNewColor().getBlue()));
 
@@ -104,9 +111,25 @@ public class SVGView {
       }
     }
 
-    result.append("</svg");
+    result.append("</svg>");
+  }
+
+  @Override
+  public String toString() {
+    return this.result.toString();
   }
 
 
+  public static void main(String[] args) throws FileNotFoundException {
+    AnimationBuilder<IAnimatorModel> builder = new Builder();
+    var fileName = "src/cs5004/animator/demo.txt";
+    Readable in = new FileReader(fileName);
+    IAnimatorModel animation = parseFile(in, builder);
+
+    SVGView svg = new SVGView();
+    svg.buildSVG(animation);
+    System.out.println(svg.toString());
+
+  }
 
 }
