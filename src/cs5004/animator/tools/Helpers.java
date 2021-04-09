41918e77af5f8a341a2bd6e2be73d71e3784d9e4
cerulij <Polik010290>
2021-04-09 12:22:50 -0400

    Update

--- src/cs5004/animator/tools/Helpers.java
+++ src/cs5004/animator/tools/Helpers.java
@@ -3,10 +3,18 @@ package cs5004.animator.tools;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Objects;
 
 public class Helpers {
 
   public static void createFile(String name, String format, String content) {
+    if (name == null || name.isBlank()) {
+      throw new IllegalArgumentException("Invalid name");
+    } else if (format == null || format.isBlank()) {
+      throw new IllegalArgumentException("Invalid format");
+    } else if (content == null || content.isBlank()) {
+      throw new IllegalArgumentException("Invalid content");
+    }
 
     String finalName = name + "." + format;
 
@@ -20,6 +28,8 @@ public class Helpers {
   }
 
   public static String[] parseCommands(String[] args) {
+    Objects.requireNonNull(args, "Must have non-null args source");
+
     String[] commands = new String[4];
     for (int i = 0; i < 4; i++) {
       commands[i] = "";
