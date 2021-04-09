f58608e62c332ecb6b3fa83ac490a3cde6170af0
cerulij <Polik010290>
2021-04-09 10:14:50 -0400

    Update

--- src/cs5004/animator/tools/FileCreator.java
+++ src/cs5004/animator/tools/FileCreator.java
@@ -3,15 +3,16 @@ package cs5004.animator.tools;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
 public class FileCreator {
 
-  public static void createFile(String name, String format, String content) throws IOException {
+  public static void createFile(String name, String format, String content) {
 
     String finalName = name + "." + format;
 
     try {
-      FileWriter newFile = new FileWriter(finalName);
+      Writer newFile = new FileWriter(finalName);
       newFile.write(content);
       newFile.close();
     } catch (IOException e) {
