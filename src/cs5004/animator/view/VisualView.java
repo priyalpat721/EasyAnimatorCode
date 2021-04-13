8a39ea288040fa5214f7fae51359c2efd6ec3dc4
Priyal <priyalpat721@gmail.com>
2021-04-12 20:35:35 -0400

    added error message

--- src/cs5004/animator/view/VisualView.java
+++ src/cs5004/animator/view/VisualView.java
@@ -4,23 +4,29 @@ import javax.swing.*;
 
 import cs5004.animator.model.IAnimatorModel;
 
+import static cs5004.animator.tools.Helpers.showMessage;
+
 public class VisualView implements IAnimatorView {
 
   @Override
   public void create(IAnimatorModel model, int speed) {
-    Canvas canvas = new Canvas(model.getBox()[0], model.getBox()[1]
-            , model.getBox()[2], model.getBox()[3], model.getShapesAtTicks(0));
-    canvas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    int end = model.getTotalTime()[1];
-    double count = 0;
-    // controls the frame
-    while (count < end) {
-      canvas.currentView(model.getShapesAtTicks(count));
-      count = count + speed / 100.0;
-      try {
-        Thread.sleep(1);
-      } catch (InterruptedException e) {
-        Thread.currentThread().interrupt();
+    if (model.getLogOfShapes().size() == 0) {
+      showMessage("Input file is empty", 2);
+    } else {
+      Canvas canvas = new Canvas(model.getBox()[0], model.getBox()[1]
+              , model.getBox()[2], model.getBox()[3], model.getShapesAtTicks(0));
+      canvas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      int end = model.getTotalTime()[1];
+      double count = 0;
+      // controls the frame
+      while (count < end) {
+        canvas.currentView(model.getShapesAtTicks(count));
+        count = count + speed / 100.0;
+        try {
+          Thread.sleep(1);
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+        }
       }
     }
   }
