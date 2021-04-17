ee2b8fb454862ba7e12a5305197f6035e023150d
anderjuli <julioauza.a@northeastern.edu>
2021-04-17 11:33:24 -0400

    more javadocs additions

--- src/cs5004/animator/controller/AnimatorControllerImpl.java
+++ src/cs5004/animator/controller/AnimatorControllerImpl.java
@@ -10,10 +10,17 @@ import cs5004.animator.view.EditorView;
 
 import static cs5004.animator.utils.AnimationReader.parseFile;
 
-
+/**
+ * Class that implements the IAnimator interface.
+ * It represents the operations managed by EasyAnimator and enables user to control animations manually.
+ */
 public class AnimatorControllerImpl implements IAnimatorController {
   private EditorView view;
 
+  /**
+   * Constructor for the AnimatorControllerImpl class.
+   * @param view data passed to the controller.
+   */
   public AnimatorControllerImpl(EditorView view) {
     this.view = view;
   }
@@ -24,6 +31,12 @@ public class AnimatorControllerImpl implements IAnimatorController {
     view.setCommandButtonListener(mouse);
     view.makeVisible();
   }
+
+  /**
+   * Main method that handles input from the view and creates a new animation.
+   * @param args default for the main method.
+   * @throws FileNotFoundException when passed a null model into readable.
+   */
 
   public static void main(String[] args) throws FileNotFoundException {
     AnimationBuilder<IAnimatorModel> builder = new Builder();
@@ -36,3 +49,5 @@ public class AnimatorControllerImpl implements IAnimatorController {
   }
 
 }
+
+
