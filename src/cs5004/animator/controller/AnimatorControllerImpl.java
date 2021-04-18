631d401637575323c194b27f70ea8e7bd15d6c44
cerulij <Polik010290>
2021-04-18 19:58:34 -0400

    Update

--- src/cs5004/animator/controller/AnimatorControllerImpl.java
+++ src/cs5004/animator/controller/AnimatorControllerImpl.java
@@ -1,14 +1,7 @@
 package cs5004.animator.controller;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-
-import cs5004.animator.model.IAnimatorModel;
-import cs5004.animator.utils.AnimationBuilder;
-import cs5004.animator.utils.Builder;
 import cs5004.animator.view.EditorView;
 
-import static cs5004.animator.utils.AnimationReader.parseFile;
 
 /**
  * Class that implements the IAnimator interface.
