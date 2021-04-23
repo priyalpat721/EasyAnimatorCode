64a82a121dbd2d818a71037231178f95685a90b7
cerulij <Polik010290>
2021-04-23 18:32:01 -0400

    update

--- test/IAnimatorControllerTest.java
+++ test/IAnimatorControllerTest.java
@@ -1,170 +1,181 @@
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.awt.event.KeyEvent;
-//import java.awt.event.KeyListener;
-//import java.awt.event.MouseListener;
-//import java.io.ByteArrayOutputStream;
-//import java.io.OutputStream;
-//import java.io.PrintStream;
-//
-//import javax.swing.JScrollPane;
-//
-//import cs5004.animator.controller.AnimatorControllerImpl;
-//import cs5004.animator.controller.IAnimatorController;
-//import cs5004.animator.model.AnimatorModelImpl;
-//import cs5004.animator.model.IAnimatorModel;
-//import cs5004.animator.view.EditorView;
-//import cs5004.animator.view.Frame;
-//import cs5004.animator.view.ShapesPanel;
-//
-//import static org.junit.Assert.assertEquals;
-//
-///**
-// * A JUnit test for the IAnimatorController.
-// */
-//public class IAnimatorControllerTest {
-//  private IAnimatorModel model;
-//  private MockView view;
-//  private IAnimatorController controller;
-//  private PrintStream originalOut;
-//  private OutputStream newOut;
-//
-//  @Before
-//  public void setUp() {
-//    model = new AnimatorModelImpl();
-//    view = new MockView();
-//    controller = new AnimatorControllerImpl(model, view, 1);
-//
-//    originalOut = System.out;
-//    newOut = new ByteArrayOutputStream();
-//    System.setOut(new PrintStream(newOut));
-//
-//    controller.go();
-//  }
-//
-//  @After
-//  public void resetStream() {
-//    // reset System.out
-//    System.setOut(originalOut);
-//  }
-//
-//  @Test
-//  public void testKeyboard() {
-//    // play: key enter
-//    KeyEvent enter = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
-//            System.currentTimeMillis(), 0,  KeyEvent.VK_ENTER, '\n');
-//    // pause: key spacebar
-//    KeyEvent space = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
-//            System.currentTimeMillis(), 0,  KeyEvent.VK_SPACE, '\b');
-//    // resume: key 's'
-//    KeyEvent keyS = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
-//            System.currentTimeMillis(), 0,  KeyEvent.VK_S, 's');
-//    // restart: key 'r'
-//    KeyEvent keyR = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
-//            System.currentTimeMillis(), 0,  KeyEvent.VK_R, 'r');
-//    // loop: key 'l'
-//    KeyEvent keyL = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
-//            System.currentTimeMillis(), 0,  KeyEvent.VK_L, 'l');
-//    // increase speed: key '.'
-//    KeyEvent period = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
-//            System.currentTimeMillis(), 0,  KeyEvent.VK_PERIOD, '.');
-//    // decrease speed: key ','
-//    KeyEvent comma = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
-//            System.currentTimeMillis(), 0,  KeyEvent.VK_COMMA, ',');
-//
-//    // press keys
-//    view.getFrame().getKeyListeners()[0].keyPressed(enter);
-//    view.getFrame().getKeyListeners()[0].keyPressed(space);
-//    view.getFrame().getKeyListeners()[0].keyPressed(keyS);
-//    view.getFrame().getKeyListeners()[0].keyPressed(keyR);
-//    view.getFrame().getKeyListeners()[0].keyPressed(keyL);
-//    view.getFrame().getKeyListeners()[0].keyPressed(period);
-//    view.getFrame().getKeyListeners()[0].keyPressed(comma);
-//
-//    assertEquals("play\npause\nresume\nrestart\nloop\nincreaseSpeed\ndecreaseSpeed\n",
-//            newOut.toString());
-//  }
-//
-//  /**
-//   * A private mock view class to test the animator controller.
-//   */
-//  private class MockView extends EditorView {
-//    private final Frame frame;
-//    private final ShapesPanel animation;
-//    private final JScrollPane scrollPane;
-//
-//    /**
-//     * Constructs a mock view object.
-//     */
-//    private MockView() {
-//      this.animation = new ShapesPanel(model.getShapesAtTicks(0));
-//      this.scrollPane = new JScrollPane(animation);
-//      this.frame = new Frame(1, 1, animation, scrollPane);
-//    }
-//
-//    @Override
-//    public void create(IAnimatorModel model, int speed) {
-//    }
-//
-//    @Override
-//    public void play() {
-//      System.out.println("play");
-//    }
-//
-//    @Override
-//    public void pause() {
-//      System.out.println("pause");
-//    }
-//
-//    @Override
-//    public void restart() {
-//      System.out.println("restart");
-//    }
-//
-//    @Override
-//    public void resume() {
-//      System.out.println("resume");
-//    }
-//
-//    @Override
-//    public void loop() {
-//      System.out.println("loop");
-//    }
-//
-//    @Override
-//    public void increaseSpeed() {
-//      System.out.println("increaseSpeed");
-//    }
-//
-//    @Override
-//    public void decreaseSpeed() {
-//      System.out.println("decreaseSpeed");
-//    }
-//
-//    @Override
-//    public void makeVisible() {
-//    }
-//
-//    @Override
-//    public void setCommandButtonListener(MouseListener mouseEvent, KeyListener keyEvent) {
-//      frame.addKeyListener(keyEvent);
-//    }
-//
-//    @Override
-//    public void setFocus() {
-//      frame.requestFocus();
-//    }
-//
-//    /**
-//     * Gets the frame of the animation.
-//     * @return the frame of the animation.
-//     */
-//    public Frame getFrame() {
-//      return this.frame;
-//    }
-//  }
-//
-//}
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import javax.swing.JScrollPane;
+
+import cs5004.animator.controller.AnimatorControllerImpl;
+import cs5004.animator.controller.IAnimatorController;
+import cs5004.animator.model.IAnimatorModel;
+import cs5004.animator.utils.AnimationBuilder;
+import cs5004.animator.utils.Builder;
+import cs5004.animator.view.Frame;
+import cs5004.animator.view.IAnimatorView;
+import cs5004.animator.view.PlayBack;
+import cs5004.animator.view.ShapesPanel;
+
+import static cs5004.animator.utils.AnimationReader.parseFile;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * A JUnit test for the IAnimatorController.
+ */
+public class IAnimatorControllerTest {
+  private IAnimatorModel model;
+  private MockView view;
+  private IAnimatorController controller;
+  private PrintStream originalOut;
+  private OutputStream newOut;
+
+  @Before
+  public void setUp() throws FileNotFoundException {
+    AnimationBuilder<IAnimatorModel> builder = new Builder();
+    Readable in = new FileReader("test/smalldemo.txt");
+    model = parseFile(in, builder);
+    view = new MockView();
+    String[] args = {"-in", "in", "-view", "playback"};
+
+    originalOut = System.out;
+    newOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(newOut));
+
+    controller = new MockController(args, model, view);
+    controller.go();
+  }
+
+  @After
+  public void resetStream() {
+    // reset System.out
+    System.setOut(originalOut);
+  }
+
+  @Test
+  public void testKeyboard() {
+    // play: key enter
+    KeyEvent enter = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
+            System.currentTimeMillis(), 0,  KeyEvent.VK_ENTER, '\n');
+    // pause: key spacebar
+    KeyEvent space = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
+            System.currentTimeMillis(), 0,  KeyEvent.VK_SPACE, '\b');
+    // resume: key 's'
+    KeyEvent keyS = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
+            System.currentTimeMillis(), 0,  KeyEvent.VK_S, 's');
+    // restart: key 'r'
+    KeyEvent keyR = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
+            System.currentTimeMillis(), 0,  KeyEvent.VK_R, 'r');
+    // loop: key 'l'
+    KeyEvent keyL = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
+            System.currentTimeMillis(), 0,  KeyEvent.VK_L, 'l');
+    // increase speed: key '.'
+    KeyEvent period = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
+            System.currentTimeMillis(), 0,  KeyEvent.VK_PERIOD, '.');
+    // decrease speed: key ','
+    KeyEvent comma = new KeyEvent(view.getFrame(), KeyEvent.KEY_PRESSED,
+            System.currentTimeMillis(), 0,  KeyEvent.VK_COMMA, ',');
+
+    // press keys
+    view.getFrame().getKeyListeners()[0].keyPressed(enter);
+    view.getFrame().getKeyListeners()[0].keyPressed(space);
+    view.getFrame().getKeyListeners()[0].keyPressed(keyS);
+    view.getFrame().getKeyListeners()[0].keyPressed(keyR);
+    view.getFrame().getKeyListeners()[0].keyPressed(keyL);
+    view.getFrame().getKeyListeners()[0].keyPressed(period);
+    view.getFrame().getKeyListeners()[0].keyPressed(comma);
+
+    assertEquals("play\npause\nresume\nrestart\nloop\nincreaseSpeed\ndecreaseSpeed\n",
+            newOut.toString());
+  }
+
+  private class MockController extends AnimatorControllerImpl {
+    private MockView view;
+
+    public MockController(String[] args, IAnimatorModel model, IAnimatorView view) {
+      super(args, model, view);
+      this.view = (MockView) view;
+    }
+
+    @Override
+    public void play() {
+      System.out.println("play");
+    }
+
+    @Override
+    public void pause() {
+      System.out.println("pause");
+    }
+
+    @Override
+    public void resume() {
+      System.out.println("resume");
+    }
+
+    @Override
+    public void restart() {
+      System.out.println("restart");
+    }
+
+    @Override
+    public void loop() {
+      System.out.println("loop");
+    }
+
+    @Override
+    public void decreaseSpeed() {
+      System.out.println("decreaseSpeed");
+    }
+
+    @Override
+    public void increaseSpeed() {
+      System.out.println("increaseSpeed");
+    }
+
+  }
+
+
+  /**
+   * A private mock view class to test the animator controller.
+   */
+  private class MockView extends PlayBack {
+    private final Frame frame;
+    private final ShapesPanel animation;
+    private final JScrollPane scrollPane;
+
+    /**
+     * Constructs a mock view object.
+     */
+    private MockView() {
+      this.animation = null;
+      this.scrollPane = new JScrollPane(null);
+      this.frame = new Frame(1, 1, animation, scrollPane);
+    }
+
+    @Override
+    public void makeVisible() {
+    }
+
+    @Override
+    public void setCommandButtonListener(MouseListener mouseEvent, KeyListener keyEvent) {
+      frame.addKeyListener(keyEvent);
+    }
+
+    @Override
+    public void setFocus() {
+      frame.requestFocus();
+    }
+
+    public Frame getFrame() {
+      return this.frame;
+    }
+  }
+
+}
 
